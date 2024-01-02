41b514757455197419325b43075e5fb4e0fc6e5f
Kevin Vargas <TriGzYT@gmail.com>
2024-01-01 22:34:24 -0500

    Completed Part 2

--- script.js
+++ script.js
@@ -17,5 +17,52 @@ try {
   console.log(`Counter value when stack overflow occurred: ${counter}`);
 }
 
+// Part 2: Trampolines
 
 
+ // Recursive function to flatten nested arrays
+ 
+const flattenRecursive = (arr, result = []) => {
+    for (const element of arr) {
+      if (Array.isArray(element)) {
+        flattenRecursive(element, result);
+      } else {
+        result.push(element);
+      }
+    }
+    return result;
+  };
+  
+  // Function for trampolining
+
+  const flattenTrampolined = (arr, result = []) => {
+    const flattenStep = (arr, result) => {
+      for (const element of arr) {
+        if (Array.isArray(element)) {
+          return () => flattenStep(element, result);
+        } else {
+          result.push(element);
+        }
+      }
+      return result;
+    };
+  
+    return () => flattenStep(arr, result);
+  };
+  
+  
+   // Create a trampoline function
+
+  const trampoline = (f, ...args) => {
+    let result = f(...args);
+    while (typeof result === "function") {
+      result = result();
+    }
+    return result;
+  };
+  
+   
+  const nestedArray = [1, [2, [3, 4, [5, 6]], 7], 8, [9, 10]];
+  const flattenedArray = trampoline(flattenTrampolined(nestedArray));
+  console.log(flattenedArray);
+  
